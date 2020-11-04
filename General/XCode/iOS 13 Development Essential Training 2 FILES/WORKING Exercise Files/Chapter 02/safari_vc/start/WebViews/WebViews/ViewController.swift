d0e3fab88ef131f7256ed54d781b68528605148f
jxakellis <68080954+jxakellis@users.noreply.github.com>
2020-11-03 20:11:08 -0600

    iOS 13 Development Essential Training: 2 Progress
    Worked on chapters 1, 2, and 3 to this Linked In Learning course. This course will help with mastering view controllers (alert view controller, sharing info with other apps, activity indicators, + more), displaying web content, collection views, multi-view templates, and info about publishing an app. Mainly pursured this course due to more information about table views, but it provides very helpful information about other topics.

---
+++ General/XCode/iOS 13 Development Essential Training 2 FILES/WORKING Exercise Files/Chapter 02/safari_vc/start/WebViews/WebViews/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  WebViews
+//
+//  Created by Todd Perkins on 9/27/19.
+//  Copyright © 2019 Todd Perkins. All rights reserved.
+//
+
+import UIKit
+import SafariServices
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+    
+    @IBAction func didPressButton(_ sender: Any) {
+        if let url = URL(string: "https://bing.com") {
+            let safariVC = SFSafariViewController(url: url)
+            present(safariVC, animated: true, completion: nil)
+        }
+        
+        
+    }
+
+}
+
---
+++ General/XCode/iOS 13 Development Essential Training 2 FILES/WORKING Exercise Files/Chapter 03/cells/start/CollectionViews/CollectionViews/CollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCell.swift
+//  CollectionViews
+//
+//  Created by Jonathan Xakellis on 11/3/20.
+//  Copyright © 2020 Todd Perkins. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var label: UILabel!
+}
