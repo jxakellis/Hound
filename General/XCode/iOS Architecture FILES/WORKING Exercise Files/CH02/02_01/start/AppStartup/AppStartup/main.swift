89e39365a127a64b1f38f403fe43aff1a2674721
jxakellis <68080954+jxakellis@users.noreply.github.com>
2020-11-11 21:22:37 -0600

    iOS Architecture Progress

---
+++ General/XCode/iOS Architecture FILES/WORKING Exercise Files/CH02/02_01/start/AppStartup/AppStartup/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  AppStartup
+//
+//  Created by Jonathan Xakellis on 11/11/20.
+//  Copyright © 2020 Jonathan Xakellis. All rights reserved.
+//
+
+import UIKit
+
+UIApplicationMain(CommandLine.argc, UnsafeMutableRawPointer(CommandLine.unsafeArgv).bindMemory(to: UnsafeMutablePointer<Int8>.self, capacity: Int(CommandLine.argc)), nil, NSStringFromClass(AppDelegate.self))
