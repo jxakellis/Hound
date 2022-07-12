1255264ae3f3d97b58061da484faa4546de31c4d
jxakellis <68080954+jxakellis@users.noreply.github.com>
2022-07-11 19:44:53 -0500

    File System Restructure

---
+++ Hound/Server/Database/Hound Database Scripts/Truncate and Insert Defaults.sql
@@ -0,0 +1,10 @@
+USE Hound;
+TRUNCATE TABLE users;
+TRUNCATE TABLE userConfiguration;
+TRUNCATE TABLE familyMembers;
+TRUNCATE TABLE families;
+TRUNCATE TABLE dogs;
+TRUNCATE TABLE dogLogs;
+TRUNCATE TABLE dogReminders;
+TRUNCATE TABLE userRequestLogs;
+TRUNCATE TABLE subscriptions; 
--- Non Hound Specific/Frontend/Swift Projects For Learning/iOS Architecture FILES/WORKING Exercise Files/CH06/SOLID.playground/Pages/Open Closed.xcplaygroundpage/Sources/TableViewExtension.swift
+++
@@ -1,9 +0,0 @@
-import UIKit
-
-extension UITableView {
-    public func deselectSelectedRow(animated: Bool) {
-        if let indexPathForSelectedRow = self.indexPathForSelectedRow {
-            self.deselectRow(at: indexPathForSelectedRow, animated: animated)
-        }
-    }
-}
