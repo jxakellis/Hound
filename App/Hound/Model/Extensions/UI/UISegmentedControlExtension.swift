d46cffaeedcb3b563a59fd8916623d0dcb6f873f
Jonathan Xakellis <jxakellis@gmail.com>
2022-10-13 15:15:12 -0700

    more slight restore

--- App/Hound/Model/Extensions/UI/UISegmentedControlExtension.swift
+++ App/Hound/Model/Extensions/UI/UISegmentedControlExtension.swift
@@ -46,6 +46,7 @@ extension UISegmentedControl {
                     // dark
                 case 2:
                     self.selectedSegmentIndex = 1
+                    // system/unspecified
                 default:
                     self.selectedSegmentIndex = 2
                 }
