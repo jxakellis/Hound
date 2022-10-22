e2c6268079bca7e21e062fed8f114efa3d575f11
Jonathan Xakellis <jxakellis@gmail.com>
2022-10-22 16:46:11 -0700

    node server folder format refactor

--- Server/Node/Production/main/tools/notifications/alarm/schedule.js
+++
@@ -1,9 +0,0 @@
-// for all scheduled items. Cannot create multiple seperate schedulers.
-const schedule = require('node-schedule');
-const eventEmitter = require('events').EventEmitter;
-
-eventEmitter.defaultMaxListeners = global.constant.limit.NUMBER_OF_SCHEDULED_JOBS_ALLOWED;
-
-module.exports = {
-  schedule,
-};
